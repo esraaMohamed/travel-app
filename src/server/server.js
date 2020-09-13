c18321e2eee3b091a7010e4536b431e78c5d6016
Esraa Afifi <esraa.afifi@vida.com>
2020-09-12 22:42:47 -0700

    initial commit

--- src/server/server.js
+++ src/server/server.js
@@ -1,22 +1,10 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const port = 3000;
-const fetch = require('node-fetch')
-
-const geoNamesBaseUrl = 'http://api.geonames.org/searchJSON?'
-const forcastBaseUrl = 'http://api.weatherbit.io/v2.0/forecast/daily?'
-const currentBaseUrl = 'http://api.weatherbit.io/v2.0/current?'
-const pixabayBaseUrl = 'https://pixabay.com/api/'
-const GEO_USERNAME = 'esraa.moe'
-const WEATHER_BIT_API_KEY='7f7da3d5de0d4323b67fd0d80015d905'
-const PIXABAY_API_KEY='18274975-ed25d3ee86e523605e57107f9'
-
 
 const express = require("express");
 const { getGeoData, getForcastWeather, getCurrentWeather, getPixaBayPhoto } = require("./apiHelper");
 
 const app = express();
-
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -43,8 +31,8 @@ app.get("/", (req, res) => {
   res.sendFile("dist/index.html");
 });
 
-app.listen(port, () => {
-  console.log(`App listening on localhost ${port}`);
+app.listen(3000, () => {
+  console.log(`App listening on localhost 3000`);
 });
 
 app.get('/geoData/:destination', (request, response) => {
